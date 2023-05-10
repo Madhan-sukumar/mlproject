1231d7c1c0c8c2818bff9ab6c57a3a45a3d39f11
Madhan <madhanbalasukumar@gmail.com>
2023-05-10 19:54:33 +0100

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,9 +12,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 #for defining variables, can use dataclass
 @dataclass
@@ -70,6 +72,10 @@ if __name__=="__main__":
 
     #initialising Data Transformation
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    #initiating model trainer
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
             
